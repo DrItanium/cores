4e9ead2029281e04de5265b6f7fa996eafa20327
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-06-28 15:44:44 -0700

    Added extended instruction set functionality

--- machine.go
+++ machine.go
@@ -6,26 +6,21 @@ import (
 )
 
 const (
-	RegisterCount             = 256
-	MemorySize                = 65536
-	MajorOperationGroupCount  = 8
-	PredicateRegisterIndex    = 255
-	StackPointerRegisterIndex = 254
-)
-
-type Word uint16
-type Dword uint32
-type Instruction Dword
-
-const (
+	RegisterCount            = 256
+	MemorySize               = 65536
+	MajorOperationGroupCount = 8
+	FalseRegister            = iota
+	TrueRegister
+	PredicateRegister
+	StackPointerRegister
+	// groups
 	InstructionGroupArithmetic = iota
 	InstructionGroupMove
 	InstructionGroupJump
 	InstructionGroupCompare
 	InstructionGroupMisc
-)
-
-const (
+	InstructionGroupExtended0
+	InstructionGroupExtended1
 	// arithmetic operations
 	ArithmeticOpAdd = iota
 	ArithmeticOpSub
@@ -122,6 +117,11 @@ const (
 	ErrorInvalidCompareOperation
 	ErrorInvalidMiscOperation
 	ErrorInvalidSystemCommand
+	ErrorWriteToFalseRegister
+	ErrorWriteToTrueRegister
+	ErrorEncodeByteOutOfRange
+	ErrorGroupValueOutOfRange
+	ErrorOpValueOutOfRange
 )
 
 var errorLookup = []string{
@@ -135,6 +135,110 @@ var errorLookup = []string{
 	"Illegal compare operation %d",
 	"Illegal misc operation %d",
 	"Invalid system command %d",
+	"Attempted to write %d to false register",
+	"Attempted to write %d to true register!",
+	"Specified illegal byte offset %d to encode data into",
+	"Provided group id %d is larger than the space allotted to specifying the group",
+	"Provided op id %d is larger than the space allotted to specifying the op",
+}
+
+type Word uint16
+type Dword uint32
+type Instruction Dword
+
+func (this Instruction) group() byte {
+	return byte(((this & 0x000000FF) & 0x7))
+}
+func (this Instruction) op() byte {
+	return byte(((this & 0x000000FF) & 0xF8) >> 3)
+}
+func (this Instruction) register(index int) (byte, error) {
+	switch index {
+	case 0:
+		return byte(this), nil
+	case 1:
+		return byte((this & 0x0000FF00) >> 8), nil
+	case 2:
+		return byte((this & 0x00FF0000) >> 16), nil
+	case 3:
+		return byte((this & 0xFF000000) >> 24), nil
+	default:
+		return 0, fmt.Errorf("Register index: %d is out of range!", index)
+	}
+}
+
+func (this Instruction) immediate() Word {
+	return Word((this & 0xFFFF0000) >> 16)
+}
+
+func (this *Instruction) setGroup(group byte) {
+	*this = ((*this &^ 0x7) | Instruction(group))
+}
+func (this *Instruction) setOp(op byte) {
+	*this = ((*this &^ 0xF8) | (Instruction(op) << 3))
+}
+func (this *Instruction) setByte(index int, value byte) error {
+	switch index {
+	case 1:
+		*this = ((*this &^ 0x0000FF00) | (Instruction(value) << 8))
+	case 2:
+		*this = ((*this &^ 0x00FF0000) | (Instruction(value) << 16))
+	case 3:
+		*this = ((*this &^ 0xFF000000) | (Instruction(value) << 24))
+	default:
+		return newError(ErrorEncodeByteOutOfRange, uint(index))
+	}
+	return nil
+}
+func (this *Instruction) setImmediate(value Word) {
+	*this = ((*this &^ 0xFFFF0000) | (Instruction(value) << 16))
+}
+
+type DecodedInstruction struct {
+	Group byte
+	Op    byte
+	Data  [3]byte
+}
+
+func (this Instruction) Decode() (*DecodedInstruction, error) {
+	var di DecodedInstruction
+	di.Group = this.group()
+	di.Op = this.op()
+	if value, err := this.register(1); err != nil {
+		return nil, err
+	} else {
+		di.Data[0] = value
+	}
+	if value, err := this.register(2); err != nil {
+		return nil, err
+	} else {
+		di.Data[1] = value
+	}
+	if value, err := this.register(3); err != nil {
+		return nil, err
+	} else {
+		di.Data[2] = value
+	}
+	return &di, nil
+}
+
+func (this *DecodedInstruction) SetImmediate(value Word) {
+	this.Data[1] = byte(value)
+	this.Data[2] = byte(value >> 8)
+}
+func (this *DecodedInstruction) Immediate() Word {
+	return Word((Word(this.Data[2]) << 8) | Word(this.Data[1]))
+}
+
+func (this *DecodedInstruction) Encode() *Instruction {
+	i := new(Instruction)
+	// encode group
+	i.setGroup(this.Group)
+	i.setOp(this.Op)
+	i.setByte(1, this.Data[0])
+	i.setByte(2, this.Data[1])
+	i.setByte(3, this.Data[2])
+	return i
 }
 
 type IrisError struct {
@@ -155,109 +259,114 @@ func (this IrisError) Error() string {
 	}
 }
 
-func (this Instruction) Group() byte {
-	return byte(((this & 0x000000FF) & 0x7))
-}
-func (this Instruction) Op() byte {
-	return byte(((this & 0x000000FF) & 0xF8) >> 3)
-}
-func (this Instruction) Register(index int) (byte, error) {
-	switch index {
-	case 0:
-		return byte(this), nil
-	case 1:
-		return byte((this & 0x0000FF00) >> 8), nil
-	case 2:
-		return byte((this & 0x00FF0000) >> 16), nil
-	case 3:
-		return byte((this & 0xFF000000) >> 24), nil
-	default:
-		return 0, fmt.Errorf("Register index: %d is out of range!", index)
-	}
-}
-
-func (this Instruction) Immediate() Word {
-	return Word((this & 0xFFFF0000) >> 16)
-}
-
-type CoreInit func(*Core) error
+type ExecutionUnit func(*Core, *DecodedInstruction) error
 type Core struct {
-	Gpr                [RegisterCount]Word
+	gpr                [RegisterCount]Word
 	Code               [MemorySize]Instruction
 	Data               [MemorySize]Word
 	Stack              [MemorySize]Word
 	Pc                 Word
 	advancePc          bool
 	terminateExecution bool
+	Xunits             [2]ExecutionUnit
 }
 
-func (this *Core) SetRegister(index byte, value Word) {
-	this.Gpr[index] = value
+func defaultExtendedUnit(core *Core, inst *DecodedInstruction) error {
+	return newError(ErrorInvalidInstructionGroupProvided, uint(inst.Group))
+}
+func (this *Core) SetRegister(index byte, value Word) error {
+	switch index {
+	case FalseRegister:
+		return newError(ErrorWriteToFalseRegister, uint(value))
+	case TrueRegister:
+		return newError(ErrorWriteToTrueRegister, uint(value))
+	default:
+		this.gpr[index] = value
+		return nil
+	}
 }
 func (this *Core) GetRegister(index byte) Word {
-	return this.Gpr[index]
+	return this.gpr[index]
 }
-func New(init CoreInit) (*Core, error) {
+func New() *Core {
 	var c Core
 	c.Pc = 0
 	c.advancePc = true
 	c.terminateExecution = false
-	if err := init(&c); err != nil {
-		return nil, err
-	} else {
-		return &c, nil
+	c.gpr[FalseRegister] = 0
+	c.gpr[TrueRegister] = 1
+	c.gpr[PredicateRegister] = 0
+	c.gpr[StackPointerRegister] = 0xFFFF
+	for i := 0; i < len(c.Xunits); i++ {
+		c.Xunits[i] = defaultExtendedUnit
 	}
+	return &c
 }
-func (this *Core) arithmetic(inst Instruction) error {
-	op := inst.Op()
-	switch op {
-	default:
-		return newError(ErrorInvalidArithmeticOperation, uint(op))
-	}
-}
-func (this *Core) move(inst Instruction) error {
-	op := inst.Op()
-	switch op {
-	default:
-		return newError(ErrorInvalidMoveOperation, uint(op))
-	}
-}
-func (this *Core) jump(inst Instruction) error {
-	op := inst.Op()
-	switch op {
-	default:
-		return newError(ErrorInvalidJumpOperation, uint(op))
-	}
-}
-func (this *Core) compare(inst Instruction) error {
-	op := inst.Op()
-	switch op {
-	default:
-		return newError(ErrorInvalidCompareOperation, uint(op))
-	}
-}
-func (this *Core) misc(inst Instruction) error {
-	op := inst.Op()
-	switch op {
-	default:
-		return newError(ErrorInvalidMiscOperation, uint(op))
-	}
-}
+
 func (this *Core) Dispatch(inst Instruction) error {
 	this.advancePc = true
-	group := inst.Group()
-	switch group {
-	case InstructionGroupArithmetic:
-		return this.arithmetic(inst)
-	case InstructionGroupMove:
-		return this.move(inst)
-	case InstructionGroupJump:
-		return this.jump(inst)
-	case InstructionGroupCompare:
-		return this.compare(inst)
-	case InstructionGroupMisc:
-		return this.misc(inst)
-	default:
-		return newError(ErrorInvalidInstructionGroupProvided, uint(group))
+	if di, err := inst.Decode(); err != nil {
+		return err
+	} else {
+		switch di.Group {
+		case InstructionGroupArithmetic:
+			return this.arithmetic(di)
+		case InstructionGroupMove:
+			return this.move(di)
+		case InstructionGroupJump:
+			return this.jump(di)
+		case InstructionGroupCompare:
+			return this.compare(di)
+		case InstructionGroupMisc:
+			return this.misc(di)
+		case InstructionGroupExtended0: // expansion group0
+			return this.extended0(di)
+		case InstructionGroupExtended1: // expansion group1
+			return this.extended1(di)
+		default:
+			return newError(ErrorInvalidInstructionGroupProvided, uint(di.Group))
+		}
 	}
+}
+func (this *Core) extended0(inst *DecodedInstruction) error {
+	return this.Xunits[0](this, inst)
+}
+func (this *Core) extended1(inst *DecodedInstruction) error {
+	return this.Xunits[1](this, inst)
+}
+
+func (this *Core) arithmetic(inst *DecodedInstruction) error {
+	switch inst.Op {
+	default:
+		return newError(ErrorInvalidArithmeticOperation, uint(inst.Op))
+	}
+}
+func (this *Core) move(inst *DecodedInstruction) error {
+	switch inst.Op {
+	default:
+		return newError(ErrorInvalidMoveOperation, uint(inst.Op))
+	}
+}
+func (this *Core) jump(inst *DecodedInstruction) error {
+	switch inst.Op {
+	default:
+		return newError(ErrorInvalidJumpOperation, uint(inst.Op))
+	}
+}
+func (this *Core) compare(inst *DecodedInstruction) error {
+	switch inst.Op {
+	default:
+		return newError(ErrorInvalidCompareOperation, uint(inst.Op))
+	}
+}
+func (this *Core) misc(inst *DecodedInstruction) error {
+	switch inst.Op {
+	case MiscOpSystemCall:
+		return this.SystemCall(inst)
+	default:
+		return newError(ErrorInvalidMiscOperation, uint(inst.Op))
+	}
+}
+func (this *Core) SystemCall(inst *DecodedInstruction) error {
+	return nil
 }
