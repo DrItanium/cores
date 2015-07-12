9e6910c1e414221d4c9eb4786e11b123ce0bc534
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-12 16:03:18 -0700

    Migrated memory.go to use a different package name

--- iris2/memory.go
+++ iris2/memory.go
@@ -1,39 +1,43 @@
 // definition of a memory controller
-package iris2
+package standard
 
 import (
 	"encoding/binary"
 	"fmt"
+	"github.com/DrItanium/cores/iris2"
 )
 
 const (
 	iPMask         = 0xFFFFFFFFF000000F // last four bits offset for instruction pointer
 	iPMaskEnd      = 0x000000000000000F
 	memoryMask     = 0xFFFFFFFFF0000000
-	memoryCapacity = 0x000000000FFFFFFF
+	memoryCapacity = ^memoryMask
 
 	stackMask = 0xFFFFFFFFFF000000
-	stackSize = 0x0000000000FFFFFF
+	stackSize = ^stackMask
 )
 
-func (this Word) asInstructionPointerAddress() Word {
+func asInstructionPointerAddress(this iris2.Word) iris2.Word {
 	return this &^ iPMask
 }
 
-func (this Word) asMemoryAddress() Word {
+func asMemoryAddress(this iris2.Word) iris2.Word {
 	return this &^ memoryMask
 }
-func (this Word) asStackAddress() Word {
+func asStackAddress(this iris2.Word) iris2.Word {
 	return this &^ stackMask
 }
 
 type MemoryController struct {
-	input      chan Packet
-	output     chan Packet
+	input      chan iris2.Packet
+	output     chan iris2.Packet
 	terminated bool
 	memory     [memoryCapacity]byte
 }
 
+func (this *MemoryController) Capacity() iris2.Word {
+	return iris2.Word(len(this.memory))
+}
 func NewMemoryController() (*MemoryController, error) {
 	var mc MemoryController
 	mc.output = make(chan Packet)
@@ -72,19 +76,19 @@ func groupMajor(this byte) byte {
 func groupMinor(this byte) byte {
 	return byte((this & memoryGroupMinorMask) >> 2)
 }
-func (this *MemoryController) loadInstruction(address Word) []byte {
+func (this *MemoryController) loadInstruction(address iris2.Word) []byte {
 	return this.rawLoad(address.asInstructionPointerAddress(), iPMaskEnd)
 }
-func (this *MemoryController) loadData(address, count Word) []byte {
+func (this *MemoryController) loadData(address, count iris2.Word) []byte {
 	return this.rawLoad(address.asMemoryAddress(), count)
 }
-func (this *MemoryController) rawLoad(address, count Word) []byte {
+func (this *MemoryController) rawLoad(address, count iris2.Word) []byte {
 	return this.memory[address : address+count]
 }
 
-func (this *MemoryController) storeValue(address Word, data []byte) {
+func (this *MemoryController) storeValue(address iris2.Word, data []byte) {
 	for ind, value := range data {
-		this.memory[address+Word(ind)] = value
+		this.memory[address+iris2.Word(ind)] = value
 	}
 }
 
@@ -100,8 +104,8 @@ func mctoInt32(this []byte) uint32 {
 func mctoInt64(this []byte) uint64 {
 	return binary.LittleEndian.Uint64(this)
 }
-func mctoWord(this []byte) Word {
-	return Word(binary.LittleEndian.Uint64(this))
+func toWord(this []byte) iris2.Word {
+	return iris2.Word(binary.LittleEndian.Uint64(this))
 }
 
 var sizeTranslationTable = []int{1, 2, 4, 8}
@@ -109,7 +113,7 @@ var sizeTranslationTable = []int{1, 2, 4, 8}
 func (this *MemoryController) parseInput() {
 	for !this.terminated {
 		p := <-this.input
-		var outPacket Packet
+		var outPacket iris2.Packet
 		outPacket.Error = nil
 		if !p.HasData() {
 			outPacket.Error = fmt.Errorf("Memory controller: provided command input stream is empty!")
@@ -138,7 +142,7 @@ func (this *MemoryController) parseInput() {
 					case 0:
 						switch groupMajor(cell) {
 						case memoryMajorLoad:
-							outPacket.Value = this.loadData(address, Word(sizeTranslationTable[groupMinor(cell)]))
+							outPacket.Value = this.loadData(address, iris2.Word(sizeTranslationTable[groupMinor(cell)]))
 						case memoryMajorGetInstructionPacket:
 							outPacket.Value = this.loadInstruction(address)
 						default:
