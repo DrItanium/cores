780d5705f1388d45e7f7cd84758434efc93807b7
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-12 17:02:59 -0700

    Added some other common functions

--- slice.go
+++ slice.go
@@ -20,6 +20,33 @@ func Empty(slice interface{}) (bool, error) {
 	if IsContainer(slice) {
 		return reflect.ValueOf(slice).Len() != 0, nil
 	} else {
-		return false, fmt.Errorf("Provided thing is not a slice, it is a %t", reflect.TypeOf(slice).Kind().String())
+		return false, fmt.Errorf("Provided thing is not a container, it is a %t", reflect.TypeOf(slice))
+	}
+}
+
+func First(slice interface{}) (interface{}, error) {
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.String, reflect.Array, reflect.Slice:
+		val := reflect.ValueOf(slice)
+		if val.Len() > 0 {
+			return val.Index(0), nil
+		} else {
+			return nil, nil
+		}
+	default:
+		return nil, fmt.Errorf("Provided thing is not a sliceable thing, it is a %t", reflect.TypeOf(slice))
+	}
+}
+func Rest(slice interface{}) (interface{}, error) {
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.String, reflect.Array, reflect.Slice:
+		val := reflect.ValueOf(slice)
+		if val.Len() > 0 {
+			return val.Slice(1, val.Len()), nil
+		} else {
+			return nil, nil
+		}
+	default:
+		return nil, fmt.Errorf("Provided thing is not a sliceable thing, it is a %t", reflect.TypeOf(slice))
 	}
 }
