9a0b78a85631771e88e9403295b55eb0c0cc9f3c
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-19 22:09:50 -0700

    Simplified the move table implementation

--- iris1/machine.go
+++ iris1/machine.go
@@ -503,25 +503,21 @@ func init() {
 }
 
 func arithmetic(core *Core, inst *DecodedInstruction) error {
-	if inst.Op >= ArithmeticOpCount {
-		return fmt.Errorf("Op index %d is not a valid arithmetic operation", inst.Op)
+	var arg0, arg1 Word
+	var err error
+	dest := inst.Data[0]
+	arg0 = core.Register(inst.Data[1])
+	result := Word(0)
+	invoke := arithmeticOps[inst.Op]
+	if invoke.ImmediateForm {
+		arg1 = inst.Immediate()
 	} else {
-		var arg0, arg1 Word
-		var err error
-		dest := inst.Data[0]
-		arg0 = core.Register(inst.Data[1])
-		result := Word(0)
-		invoke := arithmeticOps[inst.Op]
-		if invoke.ImmediateForm {
-			arg1 = inst.Immediate()
-		} else {
-			arg1 = core.Register(inst.Data[2])
-		}
-		if result, err = arithmeticOps[inst.Op].Invoke(arg0, arg1); err != nil {
-			return err
-		} else {
-			return core.SetRegister(dest, result)
-		}
+		arg1 = core.Register(inst.Data[2])
+	}
+	if result, err = arithmeticOps[inst.Op].Invoke(arg0, arg1); err != nil {
+		return err
+	} else {
+		return core.SetRegister(dest, result)
 	}
 }
 func swapMemory(core *Core, addr0, data0, addr1, data1 Word) error {
@@ -574,125 +570,149 @@ func (fn MoveOp) Invoke(core *Core, inst *DecodedInstruction) error {
 }
 
 var unimplementedMoveOp = func(_ *Core, _ *DecodedInstruction) error { return fmt.Errorf("Unimplemented move operation!") }
-var moveTable [32]MoveOp
+var moveTable = [32]MoveOp{
+	moveRegister,
+	swapRegisters,
+	swapRegAddr,
+	swapAddrAddr,
+	swapRegMem,
+	swapAddrMem,
+	moveOpSet,
+	load,
+	loadImm,
+	store,
+	storeAddr,
+	storeMem,
+	storeImm,
+	moveOpPush,
+	moveOpPushImm,
+	moveOpPop,
+	moveOpPeek,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+}
 
 func init() {
 	if MoveOpCount > 32 {
 		panic("Too many move operations registered! Programmer Failure!")
-	} else {
-		for i := 0; i < 32; i++ {
-			moveTable[i] = unimplementedMoveOp
-		}
-		moveTable[MoveOpMove] = func(core *Core, inst *DecodedInstruction) error {
-			dest := inst.Data[0]
-			src0 := inst.Data[1]
-			return core.SetRegister(dest, core.Register(src0))
-		}
-		moveTable[MoveOpSwap] = func(core *Core, inst *DecodedInstruction) error {
-			dest := inst.Data[0]
-			src0 := inst.Data[1]
-			r0 := core.Register(dest)
-			r1 := core.Register(src0)
-			if err := core.SetRegister(src0, r0); err != nil {
-				return err
-			}
-			if err := core.SetRegister(dest, r1); err != nil {
-				return err
-			}
-			return nil
-		}
-		moveTable[MoveOpSwapRegAddr] = func(core *Core, inst *DecodedInstruction) error {
-			dest := inst.Data[0]
-			src0 := inst.Data[1]
-			reg := core.Register(dest)
-			memaddr := core.Register(src0)
-			memcontents := core.DataMemory(memaddr)
-			return swapMemoryAndRegister(core, dest, reg, memaddr, memcontents)
-		}
-		moveTable[MoveOpSwapAddrAddr] = func(core *Core, inst *DecodedInstruction) error {
-			dest := inst.Data[0]
-			src0 := inst.Data[1]
-			addr0 := core.Register(dest)
-			addr1 := core.Register(src0)
-			mem0 := core.DataMemory(addr0)
-			mem1 := core.DataMemory(addr1)
-			return swapMemory(core, addr0, mem0, addr1, mem1)
-		}
-		moveTable[MoveOpSwapRegMem] = func(core *Core, inst *DecodedInstruction) error {
-			dest := inst.Data[0]
-			addr := inst.Immediate()
-			return swapMemoryAndRegister(core, dest, core.Register(dest), addr, core.DataMemory(addr))
-		}
 	}
+}
+func moveRegister(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	src0 := inst.Data[1]
+	return core.SetRegister(dest, core.Register(src0))
+}
+func swapRegisters(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	src0 := inst.Data[1]
+	r0 := core.Register(dest)
+	r1 := core.Register(src0)
+	if err := core.SetRegister(src0, r0); err != nil {
+		return err
+	}
+	if err := core.SetRegister(dest, r1); err != nil {
+		return err
+	}
+	return nil
+}
+func swapRegAddr(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	src0 := inst.Data[1]
+	reg := core.Register(dest)
+	memaddr := core.Register(src0)
+	memcontents := core.DataMemory(memaddr)
+	return swapMemoryAndRegister(core, dest, reg, memaddr, memcontents)
+}
+func swapAddrAddr(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	src0 := inst.Data[1]
+	addr0 := core.Register(dest)
+	addr1 := core.Register(src0)
+	mem0 := core.DataMemory(addr0)
+	mem1 := core.DataMemory(addr1)
+	return swapMemory(core, addr0, mem0, addr1, mem1)
+
+}
+func swapRegMem(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	addr := inst.Immediate()
+	return swapMemoryAndRegister(core, dest, core.Register(dest), addr, core.DataMemory(addr))
+}
+func moveOpSet(core *Core, inst *DecodedInstruction) error {
+	dest = inst.Data[0]
+	return core.SetRegister(dest, inst.Immediate())
+}
+func swapAddrMem(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	addr0 := core.Register(dest)
+	addr1 := inst.Immediate()
+	mem0 := core.DataMemory(addr0)
+	mem1 := core.DataMemory(addr1)
+	return swapMemory(core, addr0, mem0, addr1, mem1)
+
+}
+func load(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	src0 := inst.Data[1]
+	return core.SetRegister(dest, core.DataMemory(core.Register(src0)))
+}
+func loadImm(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetRegister(dest, core.DataMemory(inst.Immediate()))
+}
+
+func store(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	src0 := inst.Data[1]
+	return core.SetDataMemory(core.Register(dest), core.Register(src0))
+}
+func storeAddr(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	src0 := inst.Data[1]
+	return core.SetDataMemory(core.Register(dest), core.DataMemory(core.Register(src0)))
+}
+func storeMem(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetDataMemory(core.Register(dest), core.DataMemory(inst.Immediate()))
+}
+
+func storeImm(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetDataMemory(core.Register(dest), inst.Immediate())
+}
+func moveOpPush(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	core.Push(core.Register(dest))
+	return nil
+}
+func moveOpPushImm(core *Core, inst *DecodedInstruction) error {
+	core.Push(inst.Immediate())
+	return nil
+}
+func moveOpPop(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetRegister(dest, core.Pop())
+}
+func moveOpPeek(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetRegister(dest, core.Peek())
 }
 
 func move(core *Core, inst *DecodedInstruction) error {
-	if inst.Op >= MoveOpCount {
-		return fmt.Errorf("Op index %d is not a valid move operation", inst.Op)
-	} else {
-		dest := inst.Data[0]
-		src0 := inst.Data[1]
-		switch inst.Op {
-		case MoveOpMove:
-			return core.SetRegister(dest, core.Register(src0))
-		case MoveOpSwap:
-			r0 := core.Register(dest)
-			r1 := core.Register(src0)
-			if err := core.SetRegister(src0, r0); err != nil {
-				return err
-			} else if err := core.SetRegister(dest, r1); err != nil {
-				return err
-			} else {
-				return nil
-			}
-		case MoveOpSwapRegAddr:
-			reg := core.Register(dest)
-			memaddr := core.Register(src0)
-			memcontents := core.DataMemory(memaddr)
-			return swapMemoryAndRegister(core, dest, reg, memaddr, memcontents)
-		case MoveOpSwapAddrAddr:
-			addr0 := core.Register(dest)
-			addr1 := core.Register(src0)
-			mem0 := core.DataMemory(addr0)
-			mem1 := core.DataMemory(addr1)
-			return swapMemory(core, addr0, mem0, addr1, mem1)
-		case MoveOpSwapRegMem:
-			addr := inst.Immediate()
-			return swapMemoryAndRegister(core, dest, core.Register(dest), addr, core.DataMemory(addr))
-		case MoveOpSwapAddrMem:
-			addr0 := core.Register(dest)
-			addr1 := inst.Immediate()
-			mem0 := core.DataMemory(addr0)
-			mem1 := core.DataMemory(addr1)
-			return swapMemory(core, addr0, mem0, addr1, mem1)
-		case MoveOpSet:
-			return core.SetRegister(dest, inst.Immediate())
-		case MoveOpLoad:
-			return core.SetRegister(dest, core.DataMemory(core.Register(src0)))
-		case MoveOpLoadMem:
-			return core.SetRegister(dest, core.DataMemory(inst.Immediate()))
-		case MoveOpStore:
-			return core.SetDataMemory(core.Register(dest), core.Register(src0))
-		case MoveOpStoreAddr:
-			return core.SetDataMemory(core.Register(dest), core.DataMemory(core.Register(src0)))
-		case MoveOpStoreMem:
-			return core.SetDataMemory(core.Register(dest), core.DataMemory(inst.Immediate()))
-		case MoveOpStoreImm:
-			return core.SetDataMemory(core.Register(dest), inst.Immediate())
-		case MoveOpPush:
-			core.Push(core.Register(dest))
-			return nil
-		case MoveOpPushImmediate:
-			core.Push(inst.Immediate())
-			return nil
-		case MoveOpPop:
-			return core.SetRegister(dest, core.Pop())
-		case MoveOpPeek:
-			return core.SetRegister(dest, core.Peek())
-		default:
-			return fmt.Errorf("Programmer failure! Report it as such!")
-		}
-	}
+	return moveTable[inst.Op].Invoke(core, inst)
 }
 func jumpUpdateLink(core *Core, link byte, next, target Word) error {
 	if err := core.SetRegister(link, next); err != nil {
