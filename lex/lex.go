208af7c27a7f9254eed01135a62e772cd78cbab4
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-01 13:04:13 -0700

    Updated lex.go to contain the parsing elements

--- lex/lex.go
+++ lex/lex.go
@@ -1,7 +1,26 @@
 // generic lexing library for assembler syntax
 package lex
 
+import (
+	"fmt"
+	"text/scanner"
+)
+
 type Node struct {
 	Value string
 	Type  uint32
+}
+
+type Translator func(string) ([]Node, error)
+
+func (this Translator) Translate(input *scanner.Scanner) ([]Node, error) {
+	var nodes []Node
+	for input.Scan() {
+		nodes := append(nodes, this(input.Text())...)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	} else {
+		return nodes, nil
+	}
 }
