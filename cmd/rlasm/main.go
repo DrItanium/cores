b0ea36e30ea67a2d3e32dc45a488b03a7b70fabd
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-01 14:55:01 -0700

    Expanded parsing of symbols in the assembler carver

--- cmd/rlasm/main.go
+++ cmd/rlasm/main.go
@@ -21,7 +21,7 @@ func main() {
 			fmt.Println(line)
 			stmt := carveLine(line)
 			for ind, str := range stmt {
-				fmt.Printf("\t%d: %s\n", ind, str)
+				fmt.Printf("\t%d: %s\n", ind, str.Value)
 			}
 		}
 	}
@@ -31,36 +31,79 @@ func main() {
 	}
 }
 
-type statement []string
+type NodeType int
 
-func carveLine(line string) statement {
-	var s statement
+const (
+	TypeUnknown NodeType = iota
+	TypeEquals
+	TypeComma
+	TypeLabel
+	TypeRegister
+	TypeImmediate
+	TypeComment
+)
+
+type Node struct {
+	Value string
+	Type  NodeType
+}
+
+func (this *Node) IsComment() bool {
+	return this.Type == TypeComment
+}
+func (this *Node) IsLabel() bool {
+	return this.Type == TypeLabel
+}
+
+func (this *Node) NeedsAnalysis() bool {
+	return this.Type == TypeUnknown
+}
+
+type Statement []Node
+
+func carveLine(line string) Statement {
+	// trim the damn line first
+	captureSequence := func(nodes Statement, dat string, from, to int) Statement {
+		str := strings.TrimSpace(dat[from:to])
+		if len(str) > 0 {
+			return append(nodes, Node{Value: str, Type: TypeUnknown})
+		} else {
+			return nodes
+		}
+	}
+	data := strings.TrimSpace(line)
+	var s Statement
+	if len(data) == 0 {
+		return s
+	}
 	oldStart := 0
 	start := 0
 	// skip the strings at the beginning
-	for width := 0; start < len(line); start += width {
+	for width := 0; start < len(data); start += width {
 		var r rune
-		r, width = utf8.DecodeRuneInString(line[start:])
-		if unicode.IsSpace(r) || r == '=' || r == ',' {
-			str := strings.TrimSpace(line[oldStart:start])
-			if len(str) > 0 {
-				s = append(s, str)
-			}
+		r, width = utf8.DecodeRuneInString(data[start:])
+		if unicode.IsSpace(r) {
+			s = captureSequence(s, data, oldStart, start)
 			oldStart = start
+		} else if r == '=' {
+			s = captureSequence(s, data, oldStart, start)
+			s = append(s, Node{Value: "=", Type: TypeEquals})
+			oldStart = start + width
+		} else if r == ',' {
+			s = captureSequence(s, data, oldStart, start)
+			s = append(s, Node{Value: ",", Type: TypeComma})
+			oldStart = start + width
 		} else if r == ';' {
-			// consume the rest of the line
-			preComment := strings.TrimSpace(line[oldStart:start])
-			if len(preComment) > 0 {
-				s = append(s, preComment)
-			}
+			// consume the rest of the data
+			s = captureSequence(s, data, oldStart, start)
 			// then capture the comment
-			s = append(s, line[start:])
+			s = append(s, Node{Value: data[start:], Type: TypeComment})
 			oldStart = start
 			break
 		}
 	}
 	if oldStart < start {
-		s = append(s, line[oldStart:])
+		s = append(s, Node{Value: data[oldStart:]})
 	}
 	return s
 }
