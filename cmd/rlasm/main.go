f0ce49cfc6b2bb033460b39c53ab9ea4e6ed3885
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-09 14:19:34 -0700

    Updated rlasm to use the keyword parser

--- cmd/rlasm/main.go
+++ cmd/rlasm/main.go
@@ -4,10 +4,24 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/DrItanium/cores/iris1"
 	"github.com/DrItanium/cores/lisp"
+	"github.com/DrItanium/cores/parse/keyword"
 	"os"
 )
 
+var keywords *keyword.Parser
+
+func init() {
+	keywords = keyword.New()
+	keywords.AddKeyword("add")
+	keywords.AddKeyword("addi")
+	keywords.AddKeyword("label")
+	keywords.AddKeyword("sub")
+	for i := 0; i < iris1.RegisterCount; i++ {
+		keywords.AddKeyword(fmt.Sprintf("r%d", i))
+	}
+}
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	if list, err := lisp.Parse(in); err != nil {
@@ -15,4 +29,8 @@ func main() {
 	} else {
 		fmt.Println(list)
 	}
+}
+
+func IsKeyword(atom lisp.Atom) bool {
+	return keywords.IsKeyword(atom.String())
 }
