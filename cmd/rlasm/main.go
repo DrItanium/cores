fb0cd9f25f62a69c2e8201966cf7593b5326f8b3
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-09 12:29:21 -0700

    Expanded lisp parser to catch unbalanced cases

--- cmd/rlasm/main.go
+++ cmd/rlasm/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"os"
 	"strings"
@@ -10,7 +11,7 @@ import (
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
-	if list, err := New(in); err != nil {
+	if list, err := Parse(in); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println(list)
@@ -33,19 +34,68 @@ func (this List) String() string {
 	str := strings.Join(strs, " ")
 	return fmt.Sprintf("(%s)", str)
 }
-func New(buf *bufio.Reader) (List, error) {
+func ParseString(input string) (List, error) {
+	return Parse(bufio.NewReader(strings.NewReader(input)))
+}
+func Parse(buf *bufio.Reader) (List, error) {
+	q := newListParser()
+	if result, err := q.parse(buf); err != nil {
+		return nil, err
+	} else if q.hasUnfinishedLists() {
+		return nil, fmt.Errorf("Opening paren found without corresponding closing paren!")
+	} else {
+		return result, nil
+	}
+}
+
+type listParser struct {
+	depthStack *list.List
+	depth      uint64
+}
+
+func newListParser() *listParser {
+	return &listParser{depthStack: list.New(), depth: 0}
+}
+func (this *listParser) hasUnfinishedLists() bool {
+	return this.depth != 0
+}
+func (this *listParser) openList() error {
+	oldDepth := this.depth
+	this.depthStack.PushFront(this.depth)
+	this.depth++
+	if this.depth < oldDepth {
+		return fmt.Errorf("parsing stack overflow!")
+	} else {
+		return nil
+	}
+}
+func (this *listParser) closeList() error {
+	if this.depthStack.Len() == 0 {
+		return fmt.Errorf("parsing stack underflow!")
+	} else {
+		this.depthStack.Remove(this.depthStack.Front())
+		this.depth--
+		return nil
+	}
+}
+func (this *listParser) parse(buf *bufio.Reader) (List, error) {
 	var l List
 	var container Atom
 	for c, err := buf.ReadByte(); err == nil; c, err = buf.ReadByte() {
 		switch c {
 		case '(':
-			if val, err0 := New(buf); err0 != nil {
+			// push the paren to the stack
+			if err := this.openList(); err != nil {
+				return nil, err
+			} else if val, err0 := this.parse(buf); err0 != nil {
 				return nil, err0
 			} else {
 				l = append(l, val)
 			}
 		case ')':
-			if len(container) > 0 {
+			if err := this.closeList(); err != nil {
+				return nil, err
+			} else if len(container) > 0 {
 				nContainer := make(Atom, len(container))
 				copy(nContainer, container)
 				l = append(l, nContainer)
---
+++ cmd/rlasm/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+var inputs = []struct {
+	Value      string
+	ShouldFail bool
+}{
+	{"(+ 1 2 3 4) (", true},
+	{"(- 1 2 3 4) )", true},
+	{"(* 1 2 3 4)", false},
+}
+
+func parseLispString(t *testing.T, index int) {
+	if index >= len(inputs) {
+		t.Errorf("Invalid parsing input %d", index)
+		t.Fail()
+	}
+
+	if targ, err := ParseString(inputs[index].Value); err != nil {
+		if inputs[index].ShouldFail {
+			t.Logf("Expected failure occurred! %s", err)
+		} else {
+			t.Error(err)
+			t.Fail()
+		}
+	} else {
+		t.Logf("Parsed as: %s", targ)
+	}
+}
+func Test_ParseLispStrings(t *testing.T) {
+	for i := 0; i < len(inputs); i++ {
+		parseLispString(t, i)
+	}
+}
