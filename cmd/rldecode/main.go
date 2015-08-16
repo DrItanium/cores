d695d345cc54ed09857d4b54e049353bd2a214e3
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-16 16:01:22 -0700

    Added basic rldecode

---
+++ cmd/rldecode/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/DrItanium/cores/iris1"
+	"github.com/DrItanium/cores/translation"
+	"io"
+	"os"
+)
+
+var target = flag.String("target", "", "Core to target")
+var backends map[string]translation.Decoder
+
+func supportedBackends() {
+	fmt.Println("Supported backends:")
+	for key, _ := range backends {
+		fmt.Println("\t- ", key)
+	}
+}
+func init() {
+	backends = make(map[string]translation.Decoder)
+	backends["iris1"] = iris1.GetDecoder()
+
+	// this should always be the last part of this init function
+	if len(backends) == 0 {
+		panic("No backends specified!")
+	}
+}
+func main() {
+	var rawIn io.Reader
+	flag.Parse()
+	if *target == "" {
+		flag.Usage()
+		supportedBackends()
+		return
+	} else {
+		rawIn = os.Stdin
+		if dec, ok := backends[*target]; ok {
+			if lst, err := dec.Decode(rawIn); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(lst)
+			}
+		} else {
+			fmt.Printf("ERROR: unknown target %s\n", *target)
+			supportedBackends()
+		}
+	}
+}
