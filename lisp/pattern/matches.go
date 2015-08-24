7bd8768382b2d01b335bf5c83e1f900a20cc0707
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-08-23 17:13:10 -0700

    Added more stuff

--- lisp/pattern/matches.go
+++ lisp/pattern/matches.go
@@ -1,18 +1,22 @@
 package pattern
 
+import (
+	"strings"
+)
+
 type Match interface {
 	Priority() int
-	Invoke(value []byte) bool
+	Invoke(value string) bool
 }
 
-type MatchBody func([]byte) bool
+type MatchBody func(string) bool
 
 type MatchFunction struct {
 	fn       MatchBody
 	priority int
 }
 
-func (this *MatchFunction) Invoke(value []byte) bool {
+func (this *MatchFunction) Invoke(value string) bool {
 	return this.fn(value)
 }
 
@@ -22,4 +26,28 @@ func (this *MatchFunction) Priority() int {
 
 func NewMatch(fn MatchBody, priority int) Match {
 	return &MatchFunction{fn: fn, priority: priority}
+}
+
+type literalMatchFunction struct {
+	against  string
+	priority int
+}
+
+func (this *literalMatchFunction) Priority() int {
+	return this.priority
+}
+func (this *literalMatchFunction) Invoke(value string) bool {
+	return this.against == value
+}
+
+func NewLiteralMatch(literal string, priority int) Match {
+	return &literalMatchFunction{against: literal, priority: priority}
+}
+
+var SinglefieldMatch, MultifieldMatch, OptionalfieldMatch Match
+
+func init() {
+	SinglefieldMatch = NewMatch(func(v string) bool { return strings.HasPrefix(v, SinglefieldVariablePrefix) }, 0)
+	MultifieldMatch = NewMatch(func(v string) bool { return strings.HasPrefix(v, MultifieldVariablePrefix) }, 0)
+	OptionalfieldMatch = NewMatch(func(v string) bool { return strings.HasPrefix(v, OptionalVariablePrefix) }, 0)
 }
