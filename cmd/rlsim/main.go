c5339cea4ef48df712a9d3794d2e7df7bd32eeda
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-12-21 14:41:55 -0800

    Cleaned up rlsim

--- cmd/rlsim/main.go
+++ cmd/rlsim/main.go
@@ -2,11 +2,10 @@
 package main
 
 import (
-	"encoding/binary"
 	"flag"
 	"fmt"
+	"github.com/DrItanium/cores/registration"
 	"github.com/DrItanium/edgeworth"
-	"github.com/DrItanium/registration"
 	"io"
 	"os"
 )
@@ -20,6 +19,10 @@ func main() {
 	if len(flag.Args()) != 1 {
 		flag.Usage()
 		fmt.Println("Only one input file accepted!")
+		fmt.Println("Supported targets: ")
+		for _, val := range edgeworth.RegisteredMachines() {
+			fmt.Println("\t - ", val)
+		}
 	} else if mach, err0 := edgeworth.NewMachine(flag.Args()[0]); err0 != nil {
 		fmt.Println(err0)
 	} else {
@@ -28,11 +31,10 @@ func main() {
 		done2 := make(chan error)
 		data := make(chan byte)
 
-		loadMemoryImage(flag.Args()[0], done, data)
+		loadMemoryImage(flag.Args()[1], done, data)
 		go func(mach edgeworth.Machine, err chan error, data chan byte) {
 			err <- mach.InstallProgram(data)
 		}(mach, done2, data)
-		mach.InstallProgram
 		if err, err2 := <-done, <-done2; err != nil || err2 != nil {
 			if err != nil {
 				fmt.Printf("ERROR during memory image load (tool side): %s\n", err)
@@ -59,7 +61,7 @@ func loadMemoryImage(path string, done chan error, data chan byte) {
 			defer file.Close()
 			var err error
 			storage := make([]byte, 1)
-			for count, err0 := file.Read(storage); err0 != io.EOF; count, err0 = file.Read(storage) {
+			for _, err0 := file.Read(storage); err0 != io.EOF; _, err0 = file.Read(storage) {
 				if err0 != nil {
 					err = err0
 					break
