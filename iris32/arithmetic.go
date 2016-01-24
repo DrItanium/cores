d2f7ce6d26b36fd4da64033e146537764b59d905
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-01-24 14:18:23 -0800

    Copied iris32 from iris16

---
+++ iris32/arithmetic.go
@@ -0,0 +1,127 @@
+package iris32
+
+import "fmt"
+
+const (
+	// arithmetic operations
+	ArithmeticOpAdd = iota
+	ArithmeticOpSub
+	ArithmeticOpMul
+	ArithmeticOpDiv
+	ArithmeticOpRem
+	ArithmeticOpShiftLeft
+	ArithmeticOpShiftRight
+	ArithmeticOpBinaryAnd
+	ArithmeticOpBinaryOr
+	ArithmeticOpBinaryNot
+	ArithmeticOpBinaryXor
+	ArithmeticOpIncrement
+	ArithmeticOpDecrement
+	ArithmeticOpDouble
+	ArithmeticOpHalve
+	ArithmeticOpAddImmediate
+	ArithmeticOpSubImmediate
+	ArithmeticOpMulImmediate
+	ArithmeticOpDivImmediate
+	ArithmeticOpRemImmediate
+	ArithmeticOpShiftLeftImmediate
+	ArithmeticOpShiftRightImmediate
+	// always last
+	ArithmeticOpCount
+)
+
+var unimplementedBinaryOp = func(a, b Word) (Word, error) { return 0, fmt.Errorf("Operation not implemented!") }
+
+type ArithmeticOp struct {
+	ImmediateForm bool
+	Fn            func(Word, Word) (Word, error)
+}
+
+func (this *ArithmeticOp) Invoke(first, second Word) (Word, error) {
+	return this.Fn(first, second)
+
+}
+
+const (
+	DivideByZeroMessage = "Divide by zero error!"
+)
+
+type binOp func(Word, Word) (Word, error)
+
+func genericDivide(a, b Word, denomIsOne, otherwise binOp) (Word, error) {
+	if b == 0 {
+		return 0, fmt.Errorf(DivideByZeroMessage)
+	} else if b == 1 {
+		return denomIsOne(a, b)
+	} else {
+		return otherwise(a, b)
+	}
+}
+func div(a, b Word) (Word, error) {
+	return genericDivide(a, b, func(a, _ Word) (Word, error) { return a, nil }, func(a, b Word) (Word, error) { return a / b, nil })
+}
+func rem(a, b Word) (Word, error) {
+	return genericDivide(a, b, func(_, _ Word) (Word, error) { return 0, nil }, func(a, b Word) (Word, error) { return a % b, nil })
+}
+
+var unimplementedArithmeticOp = ArithmeticOp{
+	ImmediateForm: false,
+	Fn:            unimplementedBinaryOp,
+}
+var arithmeticOps = [31]ArithmeticOp{
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a + b, nil }},  // add
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a - b, nil }},  // sub
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a * b, nil }},  // mul
+	ArithmeticOp{false, div},                                                  // divide
+	ArithmeticOp{false, rem},                                                  // remainder
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a << b, nil }}, // shift left
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a >> b, nil }}, // shift right
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a & b, nil }},  // binary and
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a | b, nil }},  // binary or
+	ArithmeticOp{false, func(a, _ Word) (Word, error) { return ^a, nil }},     // unary not
+	ArithmeticOp{false, func(a, b Word) (Word, error) { return a ^ b, nil }},  // binary xor
+	ArithmeticOp{false, func(a, _ Word) (Word, error) { return a + 1, nil }},  // increment
+	ArithmeticOp{false, func(a, _ Word) (Word, error) { return a - 1, nil }},  // decrement
+	ArithmeticOp{false, func(a, _ Word) (Word, error) { return a + a, nil }},  // double
+	ArithmeticOp{false, func(a, _ Word) (Word, error) { return a / 2, nil }},  // halve
+	ArithmeticOp{true, func(a, b Word) (Word, error) { return a + b, nil }},   // immediate form of add
+	ArithmeticOp{true, func(a, b Word) (Word, error) { return a - b, nil }},   // immediate form of sub
+	ArithmeticOp{true, func(a, b Word) (Word, error) { return a * b, nil }},   // immediate form of mul
+	ArithmeticOp{true, div},                                                   // immediate form of div
+	ArithmeticOp{true, rem},                                                   // immediate form of rem
+	ArithmeticOp{true, func(a, b Word) (Word, error) { return a << b, nil }},  // immediate form of shift left
+	ArithmeticOp{true, func(a, b Word) (Word, error) { return a >> b, nil }},  // immediate form of shift right
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+	unimplementedArithmeticOp,
+}
+
+func init() {
+	if ArithmeticOpCount > 32 {
+		panic("Too many arithmetic operations defined! Programmer failure!")
+	}
+}
+func arithmetic(core *Core, inst *DecodedInstruction) error {
+	var arg0, arg1 Word
+	var err error
+	dest := inst.Data[0]
+	arg0 = core.Register(inst.Data[1])
+	result := Word(0)
+	invoke := arithmeticOps[inst.Op]
+	if invoke.ImmediateForm {
+		arg1 = inst.Immediate()
+	} else {
+		arg1 = core.Register(inst.Data[2])
+	}
+	if result, err = arithmeticOps[inst.Op].Invoke(arg0, arg1); err != nil {
+		return err
+	} else {
+		return core.SetRegister(dest, result)
+	}
+}
---
+++ iris32/move.go
@@ -0,0 +1,146 @@
+// move related operations
+package iris32
+
+import "fmt"
+
+const (
+	// Move Operations
+	MoveOpMove = iota
+	MoveOpSwap
+	MoveOpSet
+	MoveOpLoad
+	MoveOpStore
+	MoveOpPush
+	MoveOpPop
+	MoveOpPeek
+	// always last
+	MoveOpCount
+)
+
+type MoveOp func(*Core, *DecodedInstruction) error
+
+func (fn MoveOp) Invoke(core *Core, inst *DecodedInstruction) error {
+	return fn(core, inst)
+
+}
+
+var unimplementedMoveOp = func(_ *Core, _ *DecodedInstruction) error { return fmt.Errorf("Unimplemented move operation!") }
+var moveTable = [32]MoveOp{
+	moveRegister,
+	swapRegisters,
+	moveOpSet,
+	load,
+	store,
+	moveOpPush,
+	moveOpPop,
+	moveOpPeek,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+	unimplementedMoveOp,
+}
+
+func init() {
+	if MoveOpCount > 32 {
+		panic("Too many move operations registered! Programmer Failure!")
+	}
+}
+func moveRegister(core *Core, inst *DecodedInstruction) error {
+	dest, src := inst.Data[0], core.Register(inst.Data[1])
+	return core.SetRegister(dest, src)
+}
+func swapRegisters(core *Core, inst *DecodedInstruction) error {
+	dest, src0 := inst.Data[0], inst.Data[1]
+	r0, r1 := core.Register(dest), core.Register(src0)
+	if err := core.SetRegister(src0, r0); err != nil {
+		return err
+	} else if err := core.SetRegister(dest, r1); err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+func moveOpSet(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetRegister(dest, inst.Immediate())
+}
+func load(core *Core, inst *DecodedInstruction) error {
+	var val Word
+	dest, addr, seg := inst.Data[0], core.Register(inst.Data[1]), segment(inst.Data[2])
+	switch seg {
+	case dataSegment:
+		val = core.DataMemory(addr)
+	case microcodeSegment:
+		val = core.MicrocodeMemory(addr)
+	case stackSegment:
+		val = core.StackMemory(addr)
+	case callSegment:
+		val = core.CallMemory(addr)
+	case ioSegment:
+		if q, err := core.IoMemory(addr); err != nil {
+			return err
+		} else {
+			val = q
+		}
+	case codeSegment:
+		return fmt.Errorf("Can't load from the code segment!")
+	default:
+		return fmt.Errorf("Attempted to load from illegal segment %d", seg)
+	}
+	return core.SetRegister(dest, val)
+}
+
+func store(core *Core, inst *DecodedInstruction) error {
+	dest, src, seg := core.Register(inst.Data[0]), core.Register(inst.Data[1]), segment(inst.Data[2])
+	switch segment(seg) {
+	case dataSegment:
+		return core.SetDataMemory(dest, src)
+	case microcodeSegment:
+		return core.SetMicrocodeMemory(dest, src)
+	case stackSegment:
+		return core.SetStackMemory(dest, src)
+	case callSegment:
+		return core.SetCallMemory(dest, src)
+	case codeSegment:
+		return fmt.Errorf("Can't write to code memory!")
+	case ioSegment:
+		return core.SetIoMemory(dest, src)
+	default:
+		return fmt.Errorf("Attempted to write to illegal segment %d", seg)
+	}
+}
+
+func moveOpPush(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	core.Push(core.Register(dest))
+	return nil
+}
+
+func moveOpPop(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetRegister(dest, core.Pop())
+}
+
+func moveOpPeek(core *Core, inst *DecodedInstruction) error {
+	dest := inst.Data[0]
+	return core.SetRegister(dest, core.Peek())
+}
+
+func move(core *Core, inst *DecodedInstruction) error {
+	return moveTable[inst.Op].Invoke(core, inst)
+}
