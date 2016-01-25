7f64e2e8f1c7b1a288fb3802356151b7bc3bb58b
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-01-24 17:38:07 -0800

    Defined a floating point unit

--- units/arithmetic/unit.go
+++ units/arithmetic/unit.go
@@ -14,7 +14,7 @@ type Unit struct {
 	operation, source0, source1 <-chan int64
 }
 
-func New(control, operation, source0, source1 <-chan int64) *Unit {
+func NewIntegerUnit(control, operation, source0, source1 <-chan int64) *Unit {
 	var this Unit
 	this.err = make(chan error)
 	this.out = make(chan int64)
@@ -48,7 +48,7 @@ var integerArithmeticOps [IntegerOpCount]func(int64, int64) (int64, error)
 func init() {
 	integerArithmeticOps[IntegerAdd] = func(a, b int64) (int64, error) { return a + b, nil }
 	integerArithmeticOps[IntegerSubtract] = func(a, b int64) (int64, error) { return a - b, nil }
-	integerArithmeticOps[IntegerMultiply] = func(a, b int64) (int64, error) { return a + b, nil }
+	integerArithmeticOps[IntegerMultiply] = func(a, b int64) (int64, error) { return a * b, nil }
 	integerArithmeticOps[IntegerDivide] = func(num, denom int64) (int64, error) {
 		if denom == 0 {
 			return 0, fmt.Errorf("Divide by zero")
@@ -106,6 +106,89 @@ func (this *Unit) body() {
 }
 
 func (this *Unit) shutdown() error {
+	if !this.running {
+		return fmt.Errorf("this unit is not currently running!")
+	} else {
+		this.running = false
+		return nil
+	}
+}
+
+type FloatUnit struct {
+	running          bool
+	err              chan error
+	out              chan float64
+	Result           <-chan float64
+	Error            <-chan error
+	Control          <-chan int64
+	operation        <-chan int64
+	source0, source1 <-chan float64
+}
+
+func NewFloatUnit(control, operation <-chan int64, source0, source1 <-chan float64) *FloatUnit {
+	var this FloatUnit
+	this.err = make(chan error)
+	this.out = make(chan float64)
+	this.Result = this.out
+	this.Error = this.err
+	this.Control = control
+	this.operation = operation
+	this.source0 = source0
+	this.source1 = source1
+	return &this
+}
+
+const (
+	FloatAdd = iota
+	FloatSubtract
+	FloatMultipy
+	FloatDivide
+	FloatOpCount
+)
+
+var floatArithmeticOps [FloatOpCount]func(float64, float64) float64
+
+func init() {
+	floatArithmeticOps[IntegerAdd] = func(a, b float64) float64 { return a + b }
+	floatArithmeticOps[IntegerSubtract] = func(a, b float64) float64 { return a - b }
+	floatArithmeticOps[IntegerMultiply] = func(a, b float64) float64 { return a * b }
+	floatArithmeticOps[IntegerDivide] = func(a, b float64) float64 { return a / b }
+}
+
+func (this *FloatUnit) Startup() error {
+	if this.running {
+		return fmt.Errorf("Arithmetic unit is already running")
+	} else {
+		this.running = true
+		go this.body()
+		go this.controlQuery()
+		return nil
+	}
+}
+
+func (this *FloatUnit) controlQuery() {
+	<-this.Control
+	if err := this.shutdown(); err != nil {
+		this.err <- err
+	}
+}
+
+func (this *FloatUnit) body() {
+	for this.running {
+		select {
+		case op := <-this.operation:
+			if op >= FloatOpCount {
+				this.err <- fmt.Errorf("Index %d is not a legal instruction index!", op)
+			} else if op < 0 {
+				this.err <- fmt.Errorf("Index %d is less than zero!", op)
+			} else {
+				this.out <- floatArithmeticOps[op](<-this.source0, <-this.source1)
+			}
+		}
+	}
+}
+
+func (this *FloatUnit) shutdown() error {
 	if !this.running {
 		return fmt.Errorf("this unit is not currently running!")
 	} else {
